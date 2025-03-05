63508131b1432fb6822898bcee721935035c505c
minho <yiz4243@gmail.com>
2025-03-05 14:20:13 +0900

    기본틀

---
+++ components/footer.tsx
@@ -0,0 +1,49 @@
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <div id="footer_top">
+          <div id="footer_top_max">
+            <div id="footer_top_text">
+              <p>
+                믿을 수 있는
+                <br />
+                판다마켓 중고거래
+              </p>
+            </div>
+            <img id="footer_top_img" src="/img/Img_home_bottom.png" alt="" />
+          </div>
+        </div>
+        <div id="footer_bottom">
+          <div id="footer_bottom_contents">
+            <div id="footer_copyright">
+              <p>©codeit - 2024</p>
+            </div>
+            <div id="footer_faq">
+              <a href="privacy/">
+                <p>Privacy Policy</p>
+              </a>
+              <a href="faq/">
+                <p>FAQ</p>
+              </a>
+            </div>
+            <div id="footer_ad">
+              <a target="_blank" href="https://www.facebook.com/">
+                <address id="facebook"></address>
+              </a>
+              <a target="_blank" href="https://x.com/">
+                <address id="twitter"></address>
+              </a>
+              <a target="_blank" href="https://www.youtube.com/">
+                <address id="yotube"></address>
+              </a>
+              <a target="_blank" href="https://www.instagram.com/">
+                <address id="instagram"></address>
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,115 @@
+import styles from "@/styles/home.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import Header from "@/components/head";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className="main">
+        {/* <!-- 상단 이미지 --> */}
+        <nav id={styles.main_nav_box}>
+          <div id={styles.main_nav_content_box}>
+            <div id="nav_top">
+              <div id={styles.main_nav_text_box}>
+                <p>일상의 모든 물건을</p>
+                <p>거래해 보세요</p>
+              </div>
+              <Link href={"/item"}>
+                <div id={styles.main_nav_link}>구경하러 가기</div>
+              </Link>
+            </div>
+            <div id={styles.main_nav_img}>
+              <Image
+                src={"/assets/main-panda.svg"}
+                alt={"mainPandaImg"}
+                width={746}
+                height={340}
+              ></Image>
+            </div>
+          </div>
+        </nav>
+        {/* <!-- 중간 머시기 --> */}
+        <section id="middle_part">
+          {/* <!-- 1번째 섹션 --> */}
+          <div className="middle_part_section">
+            <div className="middle_part_section_box">
+              <Image
+                src={"/assets/Img_home_01.svg"}
+                alt={"mainSection1Img"}
+                width={588}
+                height={444}
+              ></Image>
+              <div id="section_1_text">
+                <p>Hot item</p>
+                <p>
+                  인기 상품을
+                  <br />
+                  확인해 보세요
+                </p>
+                <p>
+                  가장 HOT한 중고거래 물품을
+                  <br />
+                  판다 마켓에서 확인해 보세요
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* <!-- 2번째 섹션 --> */}
+          <div className="middle_part_section">
+            <div
+              id="middle_part_section_box_reverse"
+              className="middle_part_section_box"
+            >
+              <Image
+                src={"/assets/Img_home_02.svg"}
+                alt={"mainSection2Img"}
+                width={588}
+                height={444}
+              ></Image>
+              <div id="section_2_text">
+                <p>Search</p>
+                <p>
+                  구매를 원하는
+                  <br />
+                  상품을 검색하세요
+                </p>
+                <p>
+                  구매하고 싶은 물품은 검색해서
+                  <br />
+                  쉽게 찾아보세요
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* <!-- 3번째 섹션 --> */}
+          <div className="middle_part_section">
+            <div className="middle_part_section_box">
+              <Image
+                src={"/assets/Img_home_03.svg"}
+                alt={"mainSection3Img"}
+                width={588}
+                height={444}
+              ></Image>
+              <div id="section_3_text">
+                <p>Register</p>
+                <p>
+                  판매를 원하는
+                  <br />
+                  상품을 등록하세요
+                </p>
+                <p>
+                  어떤 물건이든 판매하고 싶은 상품을
+                  <br />
+                  쉽게 등록하세요
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* <!-- 푸터 --> */}
+      </div>
+    </>
+  );
+}
